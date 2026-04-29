9458182214daf30c23b4228338d022fee0db9714
elviejitos11 <tu_correo_real@gmail.com>
2026-04-29 05:19:17 -0400

    server actualizado

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 const PORT = 3003;
 
 // CAMBIA ESTAS 4 COSAS
-const BOT_TOKEN = '';
+const BOT_TOKEN = '8670917706:AAG60kOoiJlL33HFfR7oj20ISkxZ27-Crb4';
 const CHAT_ID = '';
 const SUPPORT_URL = 'https://t.me/tu_soporte';
 const RULES_TEXT = `📜 Reglas del sistema
